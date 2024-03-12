bac9c1aa340aa000498319311e97627db69f545d
OpenAirKh <brizgalov.work@gmail.com>
2024-03-12 14:38:01 +0300

    second

--- main.js
+++ main.js
@@ -57,9 +57,10 @@ function salary_month(salary, month) {
 return salary * month;
 
 }
-
+let a =1;
 
 function salary_month_tax(salary, month, tax) {
+
 
     return salary * month * (1 - tax / 100);
 
