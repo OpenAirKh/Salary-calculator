a021c31b4c7e3fc997e775faeb4ccc4678c8cf4f
OpenAirKh <brizgalov.work@gmail.com>
2024-03-12 16:24:56 +0300

    delete and change let - const  string in .js file

--- salary.js
+++ salary.js
@@ -1,9 +1,10 @@
-let age = [] ;
-    console.log(typeof(age));
+
 function result_all() {
-    let salary = document.getElementById('salary').value;
-    let month = document.getElementById('month').value;
-    let totalSalary = salary_month(salary, month);
+
+
+    const salary = document.getElementById('salary').value;
+    const month = document.getElementById('month').value;
+    const totalSalary = salary_month(salary, month);
     document.getElementById('totalSalary').innerHTML = totalSalary;
 
     
@@ -12,9 +13,11 @@ function result_all() {
 function salary_tax() {
    
 
-    let salary_tax = document.getElementById('salary_tax').value;
-    let month_tax = document.getElementById('month_tax').value;
-    let tax_tax = document.getElementById('tax').value;
-    let totalSalary_tax = salary_month_tax(salary_tax, month_tax, tax_tax);
+    const salary_tax = document.getElementById('salary_tax').value;
+    const month_tax = document.getElementById('month_tax').value;
+    const tax_tax = document.getElementById('tax').value;
+    const totalSalary_tax = salary_month_tax(salary_tax, month_tax, tax_tax);
     document.getElementById('totalSalary_tax').innerHTML = totalSalary_tax;
+   
 }
+
